9d88dc9f7c9808f7bb3ce1c4dfcd72dd0ffcfaba
Fabrizio Maccioni <fabriziomaccioni@forwardnetworks.com>
2019-09-18 22:25:57 -0700

    Remove hardcode Firewall IP address and get it from the platform

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ pipeline {
                 echo "Getting Path info using Ansible URI module"
                 sh "ansible-playbook get_path.yml -vvvvv"
                 echo "Checking if routing and policies are already in place for the given path"
-                sh "python is_change_needed.py"
+                sh "ansible-playbook is_change_needed.yml -vvvvv"
                 echo "currentBuild.currentResult: ${currentBuild.currentResult}"
             }
         }
--- is_change_needed.py
+++ is_change_needed.py
@@ -2,6 +2,35 @@
 
 import json
 import os
+import requests
+import argparse
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "url",
+    help="The URL of the deviceSource endpoint")
+parser.add_argument(
+    "username",
+    help="Forward Networks instance username")
+parser.add_argument(
+    "password",
+    help="Forward Networks instance username")
+parser.add_argument(
+    "--verify",
+    help="Whether to verify the certificate on the instance "
+         "(e.g. True or False)",
+    action="store_true")
+
+args = parser.parse_args()
+
+# The patch APIs returns device names.
+# This function translates device name into device ip 
+def get_device_ip(device_name):
+    device_list = requests.get(args.url, auth=(args.username, args.password), verify=args.verify).json()
+    for device in device_list:
+        if device["name"] == device_name:
+            return device["host"]
 
 # Open the paths.json that contains the Path API json output
 try:
@@ -11,12 +40,9 @@ try:
 except:
   print("An exception occurred while opening the API json file")
 
-#initialize env variable to control Stage workflow
-envCmd = 'export JENKINS_IS_CHANGE_NEEDED=TRUE'
-os.system(envCmd)
-
 # convert the file from string to Python object format
 paths_json = json.loads(file_str)
+f.close()
 
 # Extract the first path and the related outcomes
 best_path = paths_json['info']['paths'][0]
@@ -35,16 +61,23 @@ if path_forwarding_outcome != "DELIVERED":
 
 elif path_forwarding_outcome == "DELIVERED" and path_security_outcome == "PERMITTED":
     print("The security policy is already configured. No changes needed")
-    # Set an enviroment varial to skip the remaning stages
-    envCmd = 'export JENKINS_IS_CHANGE_NEEDED=FALSE'
-    os.system(envCmd)
 
 elif path_forwarding_outcome == "DELIVERED" and path_security_outcome == "DENIED":
     print("The forwarding is OK but the security rules need to be changed on these devices: ")
+    f_ip = open("firewall_ip","w+")
+    f_name = open("firewall_name","w+")
     for hop in best_path['hops']:
         for behavior in hop['behaviors']:
             if behavior == "ACL_DENY":
+                #Save device name into file
                 print(hop['deviceName'])
+                f_name.write(hop['deviceName'])
+                #Translate device name into device ip and store it into a files
+                firewall_ip = get_device_ip(hop['deviceName'])
+                print(firewall_ip)
+                f_ip.write(firewall_ip)
+    f_ip.close()
+    f_name.close()
 
 else:
     print("Something went wrong!!")
